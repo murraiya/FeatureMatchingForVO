8ebeaad567375c20c170b9a0148e5bfafe254973
murraiya <kimsuji0407@gmail.com>
2022-09-05 23:27:41 +0900

    made it

--- feature_pkg/src/videoLoad.cpp
+++ feature_pkg/src/videoLoad.cpp
@@ -1,19 +1,58 @@
+#include <ros/ros.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
 
 using namespace cv;
 using namespace std;
 
-int main(int ac, char** av) {
+cv_bridge::CvImage cvImg;
+sensor_msgs::Image ros_img_msg;
+std_msgs::Header header;
+Mat img;
+
+
+void ROSpub(Mat& img){
+    cout<<img.empty()<<endl;
+    ros::NodeHandle nh;
+    ros::Publisher raw_image_pub = nh.advertise<sensor_msgs::ImagePtr>("/raw_image", 1);
+    cvImg.image=img;
+    cvImg.toImageMsg(ros_img_msg);
+    
+    raw_image_pub.publish(ros_img_msg);
+    cout<<"sensormsg pub"<<endl;
+};
+
+
+// cv::Mat img; // << image MUST be contained here
+// cv_bridge::CvImage img_bridge;
+// sensor_msgs::Image img_msg; // >> message to be sent
+
+// std_msgs::Header header; // empty header
+// header.seq = counter; // user defined counter
+// header.stamp = ros::Time::now(); // time
+// img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, img);
+// img_bridge.toImageMsg(img_msg); // from cv_bridge to sensor_msgs::Image
+// pub_img.publish(img_msg); // ros::Publi
+
+int loadVideo(){
 
 	VideoCapture cap("/media/autonav/SJ_SSD/forFeatureMatching.avi");
 	if (!cap.isOpened())
 	{
 		printf("Can't open the camera");
-		return -1;
+        return -1;
 	}
 
-	Mat img;
+	
 
 	while (1)
 	{
@@ -22,15 +61,27 @@ int main(int ac, char** av) {
 		if (img.empty())
 		{
 			printf("empty image");
-			return 0;
+            return 0;
 		}
-        
-		imshow("camera img", img);
 
-		if (waitKey(25) == 27)
-			break;
+        ROSpub(img);
+
+        
+		//imshow("camera img", img);
+
+		// if (waitKey(25) == 27)
+		// 	break;
 	}
 
 
-	return 0;
+}
+
+
+
+int main(int argc, char** argv) {
+
+    ros::init(argc, argv, "videoLoadNode");
+    
+    loadVideo();
+    return 0;
 }
---
+++ feature_pkg/src/videoSubVisual.cpp
@@ -0,0 +1,46 @@
+#include <ros/ros.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+
+
+void ImgSubCallback(const sensor_msgs::Image& raw_img){
+    //start here convert to cv img and imshow, check 
+    cv_bridge::CvImagePtr cv_ptr;
+    cv_ptr = cv_bridge::toCvCopy(raw_img, sensor_msgs::image_encodings::BGR8);
+    cv::imshow("image", cv_ptr->image);  //CvImage class에서 멤버변수로 cv::Mat image 있음, ptr 형식 객체의 멤버에 접근할 때 -> 연산자 쓴다.
+    cv::waitKey(30);
+
+}
+
+// cv_bridge::CvImagePtr cv_ptr;
+//     try
+//     {
+//       cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+//     }
+//     catch (cv_bridge::Exception& e)
+//     {
+//       ROS_ERROR("cv_bridge exception: %s", e.what());
+//       return;
+
+
+
+
+int main(int argc, char** argv) {
+
+    ros::init(argc, argv, "videoSubVisualNode");
+    ros::NodeHandle nh;
+    ros::Subscriber raw_image_sub = nh.subscribe("/raw_image", 10, ImgSubCallback);
+    ros::spin();
+
+
+    return 0;
+}
