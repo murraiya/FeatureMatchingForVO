eb0bfc28b6c2f7beab708b8aaac0cd069d863ea3
murraiya <kimsuji0407@gmail.com>
2022-09-07 06:33:44 +0900

    before modify - ORB BRISK SIFT store result in ws

--- feature_pkg/src/processing.cpp
+++ feature_pkg/src/processing.cpp
@@ -31,12 +31,12 @@ vector<cv::KeyPoint> TargetKeypoints, ReferenceKeypoints;
 cv::Mat TargetDescriptor, ReferDescriptor;
 
 cv::Ptr<cv::Feature2D> orb = cv::ORB::create(10);
-// cv::Ptr<cv::Feature2D> sift = cv::SIFT::create(500);
+cv::Ptr<cv::Feature2D> sift = cv::SIFT::create(500);
 cv::Ptr<cv::Feature2D> brisk = cv::BRISK::create();
 
 cv::Ptr<cv::DescriptorMatcher> Matcher_ORB = cv::BFMatcher::create(cv::NORM_HAMMING);		// Brute-Force matcher create method
 cv::Ptr<cv::DescriptorMatcher> Matcher_BRISK = cv::BFMatcher::create(cv::NORM_HAMMING);		// Brute-Force matcher create method
-// cv::Ptr<cv::DescriptorMatcher> Matcher_SIFT = cv::BFMatcher::create(cv::NORM_L2);			// Brute-Force matcher create method
+cv::Ptr<cv::DescriptorMatcher> Matcher_SIFT = cv::BFMatcher::create(cv::NORM_L2);			// Brute-Force matcher create method
 
 vector<cv::DMatch> matches;	// Class for matching keypoint descriptors.
 int cnt = 0;
@@ -73,10 +73,7 @@ void processing(cv::Mat& frame){
 
 
 
-    // 이제 타겟 이미지를 정해서 Refer frame과 feature matching을 실행할 것임
-
-    // O or o : ORB , B or b : BRISK, S or s : SIFT를 선택할 수 있도록 설정하기
-
+   
     if ((key == 84) || (key == 116)) // Target Image(T 또는 t)을 눌렀을 때
     {
         cout << "Target image으로 선택됩니다.\n\n";
@@ -87,8 +84,12 @@ void processing(cv::Mat& frame){
         imshow("Target Gray Image", Target_gray_image);
     }
 
-    if(cnt == 20)
-    //if((key == 79) || (key == 111))	// ORB를 이용, Target image 선정
+     // 이제 타겟 이미지를 정해서 Refer frame과 feature matching을 실행할 것임
+
+    // O or o : ORB , B or b : BRISK, S or s : SIFT를 선택할 수 있도록 설정하기
+
+
+    if((key == 79) || (key == 111))	// ORB를 이용, Target image 선정
     {
        
         // References image에 대해 feature 뽑기
@@ -122,8 +123,11 @@ void processing(cv::Mat& frame){
         cv::Mat Result;
         cv::drawMatches(Target_gray_image, TargetKeypoints, Refer_gray_image, ReferenceKeypoints, matches, Result, cv::Scalar::all(-1), cv::Scalar(-1), vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
         // Draws the found matches of keypoints from two images.
+       
 
         imshow("Matching Result_ORB", Result);
+        imwrite("/home/autonav/feature_ws/Matching Result_ORB.jpg", Result);
+
         cv::waitKey(0);
     }
     else if ((key == 66) || (key == 98))	// BRISK를 이용, Target image 선정
@@ -168,6 +172,70 @@ void processing(cv::Mat& frame){
         // Draws the found matches of keypoints from two images.
 
         imshow("Matching Result_BRISK", Result_BRISK);
+        imwrite("/home/autonav/feature_ws/Matching Result_BRISK.jpg", Result_BRISK);
+
+    }
+    
+    if(cnt == 20)
+    // else if ((key == 83) || (key == 115))	// SIFT를 이용, Target image 선정
+    {
+        sift->detectAndCompute(Refer_gray_image, cv::Mat(), ReferenceKeypoints, ReferDescriptor);
+
+        // Refer image에 대해 keypoint 찾은 거 보고 싶으면 주석 해제
+        /*cv::Mat sift_refer_result;
+        cv::drawKeypoints(Refer_gray_image, ReferenceKeypoints, sift_refer_result);
+        imshow("Result_sift_)resfer_result", sift_refer_result);*/
+
+        cout << "SIFT를 선택하셨습니다. 해당 이미지를 Target image으로 선택합니다.\n\n";
+        Target_image = frame; // 눌렀을 때의 해당 프레임을 Target image으로 선택
+        cv::cvtColor(Target_image, Target_gray_image, cv::COLOR_RGB2GRAY);
+
+        imshow("Target Image", Target_image);	// Target image 보여주기
+        imshow("Target Gray Image", Target_gray_image);
+
+        //sift->detect(Target_gray_image, TargetKeypoints);
+        //sift->compute(Target_gray_image, TargetKeypoints, TargetDescriptor);   // 예전 버전의 경우 이렇게 했어야 함
+        sift->detectAndCompute(Target_gray_image, cv::Mat(), TargetKeypoints, TargetDescriptor);
+
+        // Target image에 대해 keypoint 찾은 거 보고 싶으면 주석 해제
+        /*cv::Mat sift_target_result;
+        cv::drawKeypoints(Target_gray_image, TargetKeypoints, sift_target_result);
+        imshow("Result_sift_Target_result", sift_target_result);*/
+
+        // Refer image와 Target image의 fature를 이용하여 Feature matching 실행
+
+        Matcher_SIFT->match(TargetDescriptor, ReferDescriptor, matches);	// Find the best match for each descriptor from a query set.
+        Matcher_SIFT->match(ReferDescriptor, TargetDescriptor, matches);
+
+        double max_dist = 0.0, min_dist = 100.0;
+        for (int i = 0; i < matches.size(); i++)
+        {
+            double dist = matches[i].distance;
+            if (dist < min_dist)
+                min_dist = dist;
+            if (dist > max_dist)
+                max_dist = dist;
+        }
+
+        // drawing only good matches (dist less than 2*min_dist)
+        vector<cv::DMatch> good_matches;
+
+        for (int i = 0; i < matches.size(); i++)
+        {
+            if (matches[i].distance <= 2 * min_dist)
+            {
+                good_matches.push_back(matches[i]);
+            }
+        }
+
+        cv::Mat Result_SIFT;
+
+        cv::drawMatches(Target_gray_image, TargetKeypoints, Refer_gray_image, ReferenceKeypoints, good_matches, Result_SIFT, cv::Scalar::all(-1), cv::Scalar(-1), vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+        // Draws the found matches of keypoints from two images.
+
+        imshow("Matching Result_SIFT", Result_SIFT);
+        imwrite("/home/autonav/feature_ws/Matching Result_SIFT.jpg", Result_SIFT);
+
     }
 }
 
