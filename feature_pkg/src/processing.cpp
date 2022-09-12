3db8d3a83f9840bb9293a587e154150206d9bd74
murraiya <kimsuji@inha.edu>
2022-09-12 16:32:57 +0900

    ready for getiing H

--- feature_pkg/src/processing.cpp
+++ feature_pkg/src/processing.cpp
@@ -11,6 +11,8 @@ int gettingH(const vector<cv::DMatch>& good_matches){
     H = findHomography( beforeFramePt, afterFramePt, RANSAC );
     cv::recoverPose(E, points2, points1, R, t, 1400 , pp, mask);
 
+    //start here!!!!!!!!!!!!! recover pose input E F H ??
+
     return 0;
 }
 
